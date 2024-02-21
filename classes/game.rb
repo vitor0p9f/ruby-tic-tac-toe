2dfc348a71b1f1e3f36645873c52c6873a6a0268
vitor0p9f <dev.vitorpfontes002@gmail.com>
2024-02-21 15:16:59 -0300

    feat(classes/game.rb): Creating a new function
    - Creating the function responsible for initializing the game.
    - Adding an array to store previously chosen positions.
    - Capturing user input.
    - Inserting each player's shape into the chosen position.

--- classes/game.rb
+++ classes/game.rb
@@ -9,10 +9,41 @@ class Game
     @players = [].push(Player.new('X'), Player.new('O'))
     @board = Board.new
     @moves = 0
-    @current_player = ''
+    @current_player = ['Player 1', 'Player 2'].sample
+    @win_or_draw = false
+    @selected_positions = []
   end
 
-  def start; end
+  def start
+    puts "Player 1 is X and player 2 is O\n\n"
+
+    @board.draw
+
+    loop do
+      break if @win_or_draw
+
+      print "It's #{@current_player} time, choose a position: "
+
+      position = if @current_player == 'X'
+                   @players[0].read_position(@selected_positions)
+                 else
+                   @players[1].read_position(@selected_positions)
+                 end
+
+      @board.insert_shape(
+        @current_player == 'Player 1' ? @players[0].shape : @players[1].shape,
+        position
+      )
+
+      @selected_positions.push(position)
+
+      @current_player = if @current_player == 'Player 1'
+                          'Player 2'
+                        else
+                          'Player 1'
+                        end
+    end
+  end
 
   private
 
