fe877f7a63921adf4da053f3b8e45c09d27beb82
vitor0p9f <dev.vitorpfontes002@gmail.com>
2024-02-21 10:34:44 -0300

    feat(classes/board): Creating a new function
    - Creating a function that insert a shape on board.
    - Changing the visibility of board's numbers.

--- classes/board.rb
+++ classes/board.rb
@@ -4,7 +4,7 @@
 class Board
   private
 
-  attr_accessor :boards_state, :board_template
+  attr_accessor :boards_state, :board_template, :current_player, :current_board
 
   public
 
@@ -12,25 +12,37 @@ class Board
     @players = players
 
     @board_template = <<~BOARD
-      \t 01111│02222│03333
-      \t 11111│12222│13333
-      \t 21111│22222│23333
+      \t      │     │
+      \t   1  │  2  │  3
+      \t      │     │
       \t ─────┼─────┼─────
-      \t 04444│05555│06666
-      \t 14444│15555│16666
-      \t 24444│25555│26666
+      \t      │     │
+      \t   4  │  5  │  6
+      \t      │     │
       \t ─────┼─────┼─────
-      \t 07777│08888│09999
-      \t 17777│18888│19999
-      \t 27777│28888│29999
+      \t      │     │
+      \t   7  │  8  │  9
+      \t      │     │
     BOARD
 
     @board_state = [['', '', ''],
                     ['', '', ''],
                     ['', '', '']]
+
+    @current_board = @board_template
   end
 
   def draw
-    puts board_template.encode('utf-8').gsub(/[0-9]/, ' ')
+    puts @current_board.encode('utf-8')
+    puts
+  end
+
+  def insert_shape(shape, position)
+    @current_board = @current_board.gsub(
+      /#{Regexp.escape(position.to_s)}/,
+      shape
+    )
+
+    draw
   end
 end
