f357dd06a9f871ccfe3bc0ef3b087cadbe806395
vitor0p9f <dev.vitorpfontes002@gmail.com>
2024-02-20 21:01:49 -0300

    refactor(classes/board.rb): Applying KISS principle
    - Rewriting the tempĺate used to print the board on the console.
    - Creating an array to store the state of the board.

--- classes/board.rb
+++ classes/board.rb
@@ -2,30 +2,31 @@
 
 # Represents a tic-tac-toe board
 class Board
+  private
+
+  attr_accessor :boards_state, :board_template
+
+  public
+
   def initialize(players)
     @players = players
+
+    @board_template = <<~BOARD
+      \t 01111│02222│03333
+      \t 11111│12222│13333
+      \t 21111│22222│23333
+      \t ─────┼─────┼─────
+      \t 04444│05555│06666
+      \t 14444│15555│16666
+      \t 24444│25555│26666
+      \t ─────┼─────┼─────
+      \t 07777│08888│09999
+      \t 17777│18888│19999
+      \t 27777│28888│29999
+    BOARD
   end
 
   def draw
-    13.times do |number|
-      if [0, 12].include?(number)
-        puts
-        next
-      end
-
-      if [4, 8].include?(number)
-        print "\t"
-        32.times do |number|
-          if [10, 21].include?(number)
-            print "\u{253C}".encode('utf-8')
-          else
-            print "\u{2500}".encode('utf-8')
-          end
-        end
-        puts
-      else
-        puts "\t\t  \u{2502}\t     \u{2502}\t\t  ".encode('utf-8')
-      end
-    end
+    puts board_template.encode('utf-8').gsub(/[0-9]/, ' ')
   end
 end
