0e8f1699120c8c6c59604f06d47dfda077c74b42
vitor0p9f <dev.vitorpfontes002@gmail.com>
2024-02-21 15:13:40 -0300

    feat(classes/player): Updating the function that captures the desired position
    - Checking if the chosen position is not already filled on the board.

--- classes/player.rb
+++ classes/player.rb
@@ -8,12 +8,23 @@ class Player
     @shape = shape
   end
 
-  def read_position
+  def read_position(selected_positions)
     position = gets.to_i
 
-    while position < 1 || position > 9
-      print 'This isn\'t a valid position, please, choose again: '
-      position = gets.to_i
+    loop do
+      unless (1..9).include?(position)
+        print 'This isn\'t a valid position, please, choose again: '
+        position = gets.to_i
+        next
+      end
+
+      if selected_positions.include?(position)
+        print 'This position has already been chosen, please choose another one: '
+        position = gets.to_i
+        next
+      end
+
+      break
     end
 
     position
